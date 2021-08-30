3f8e3f357cb00f66c9d8a76696b886206c772850
Cassiano Raupp <cassiano.raupp@outlook.com>
2021-08-30 20:11:12 -0300

    Em desenvolvimento.

--- src/common/components/Loading/index.js
+++ src/common/components/Loading/index.js
@@ -1,11 +1,17 @@
 import CircularProgress from '@material-ui/core/CircularProgress'
+import { Fragment } from 'react'
+import { Helmet } from 'react-helmet'
 import { LextaticoLoadingBox } from "./styles"
 
 const Loading = () => {
     return (
-        <LextaticoLoadingBox>
-            <CircularProgress size={100} />
-        </LextaticoLoadingBox>
+        <Fragment>
+            <Helmet title={`${process.env.REACT_APP_TITLE} | Loading`} />
+            <LextaticoLoadingBox>
+                <CircularProgress size={100} />
+            </LextaticoLoadingBox>
+        </Fragment>
+
     );
 }
 
