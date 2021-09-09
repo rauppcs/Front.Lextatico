bbf39d563c90707093f9419017ee829f1d23f16c
Cassiano S.Raupp <cassiano.raupp@outlook.com>
2021-09-08 21:56:00 -0300

    form do forgotPassword centralizado.

--- src/pages/LogIn/styles.js
+++ src/pages/LogIn/styles.js
@@ -24,6 +24,7 @@ export const LextaticoFormContentLeft = styled(Form)({
     justifyContent: "center",
     flexDirection: "column",
     alignItems: "flex-start",
+    height: "100%",
     padding: "20px"
 });
 
