47663993a2f1752f178ed76b4c07a9d9e588c0ee
Cassiano Raupp <cassiano.raupp@outlook.com>
2021-11-22 20:36:55 -0300

    Ajustes.

--- src/common/components/loading/index.js
+++ src/common/components/loading/index.js
@@ -1,16 +1,9 @@
 import { Box, CircularProgress } from '@material-ui/core'
 import LinearProgress from '@material-ui/core/LinearProgress';
-import { Fragment, useContext, useEffect } from 'react'
+import { Fragment } from 'react'
 import { LextaticoLoadingBox } from "./styles";
-import { MyContext } from "../../../App";
 
 export const CircularLoading = ({ height, sizeLoading = 50 }) => {
-    const { setTitleName } = useContext(MyContext);
-
-    useEffect(() => {
-        setTitleName("Loading");
-    }, [setTitleName]);
-
     return (
         <Fragment>
             <LextaticoLoadingBox style={{ height: height }}>
@@ -20,13 +13,7 @@ export const CircularLoading = ({ height, sizeLoading = 50 }) => {
     );
 }
 
-export const LinearLoading = ({ height, sizeLoading = 50 }) => {
-    const { setTitleName } = useContext(MyContext);
-
-    useEffect(() => {
-        setTitleName("Loading");
-    }, [setTitleName]);
-
+export const LinearLoading = ({ height }) => {
     return (
         <Fragment>
             <Box style={{ height: height }}>
